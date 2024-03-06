2cf47bd32818043a605b277ea21abdc59499221b
Calyen93 <genco.claudio3@gmail.com>
2024-03-06 17:13:05 +0100

    Sostituito String con la classe Product

--- src/classes/Warehouse.java
+++ src/classes/Warehouse.java
@@ -4,47 +4,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Warehouse {
-    private Map<String, Integer> stock;
+    private Map<Product, Integer> stock;
 
     public Warehouse() {
         this.stock = new HashMap<>();
     }
 
     //Aggiunge una determinata quantità di un prodotto specificato al magazzino.
-    public void addProduct(String productId, int quantity) {
-        if (stock.containsKey(productId)) {
-            int currentQuantity = stock.get(productId);
-            stock.put(productId, currentQuantity + quantity);
+    public void addProduct(Product product, int quantity) {
+        if (stock.containsKey(product)) {
+            int currentQuantity = stock.get(product);
+            stock.put(product, currentQuantity + quantity);
         } else {
-            stock.put(productId, quantity);
+            stock.put(product, quantity);
         }
     }
 
     //Rimuove una determinata quantità di un prodotto specificato dal magazzino.
-    public void removeProduct(String productId, int quantity) {
-        if (stock.containsKey(productId)) {
-            int currentQuantity = stock.get(productId);
+    public void removeProduct(Product product, int quantity) {
+        if (stock.containsKey(product)) {
+            int currentQuantity = stock.get(product);
             if (currentQuantity <= quantity) {
-                stock.remove(productId);
+                stock.remove(product);
             } else {
-                stock.put(productId, currentQuantity - quantity);
+                stock.put(product, currentQuantity - quantity);
             }
         } else {
-            System.out.println("Prodotto con ID " + productId + " non trovato in magazzino.");
+            System.out.println("Prodotto: " + product + " non trovato in magazzino.");
         }
     }
 
     //restituisce la quantità disponibile di un prodotto specificato nel magazzino.
 
-    public int checkStock(String productId) {
-        return stock.getOrDefault(productId, 0);
+    public int checkStock(Product product) {
+        return stock.getOrDefault(product, 0);
     }
 
     //Stampa magazzino
     public void printStock() {
         System.out.println("Inventario del magazzino:");
-        for (Map.Entry<String, Integer> entry : stock.entrySet()) {
-            System.out.println("ID prodotto: " + entry.getKey() + ", Quantità: " + entry.getValue());
+        for (Map.Entry<Product, Integer> entry : stock.entrySet()) {
+            System.out.println("Prodotto: " + entry.getKey() + ", Quantità: " + entry.getValue());
         }
     }
 }
