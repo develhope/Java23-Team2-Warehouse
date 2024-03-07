e3400de4f2da582c004ec9091c45c5df4765255d
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-03-07 12:32:03 +0100

    Creato un metodo scanner che permetta di aggiungere prodotti alla Warehouse

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import classes.Cart;
 import classes.Product;
+import classes.Warehouse;
 import products.KindOfProduct;
 
 public class Main {
 
     public static void main(String[] args) {
-        Product product = new Product(KindOfProduct.NOTEBOOK, "Apple", "XR", "tr", "dsd", 2.3, 2, 1000, 1250, 3);
-        Product product2 = new Product(KindOfProduct.NOTEBOOK, "Apple", "XR", "tr", "dsd", 2.3, 2, 1000, 1250, 3);
+        Product product = new Product(KindOfProduct.NOTEBOOK, "Apple", "XR", "tr", "dsd", 2.3, 2, 1000, 1250);
+        Product product2 = new Product(KindOfProduct.NOTEBOOK, "Apple", "XR", "tr", "dsd", 2.3, 2, 1000, 1250);
         Cart cartTest = new Cart();
         cartTest.addToCart(product, 5);
         cartTest.removeFromCart(product, 2);
@@ -15,6 +16,10 @@ public class Main {
         cartTest.removeFromCart(product, 2);
         cartTest.removeFromCart(product, 2);
         cartTest.printProductsInCart();
+        Warehouse warehouse = new Warehouse();
+        warehouse.scnProdConstructor();
+        warehouse.printStock();
+
 
     }
 
