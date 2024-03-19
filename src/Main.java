5657ce0a7e58e13e3ee0c5a6934ebf158c9bc538
emmathebear <158595325+emmathebear@users.noreply.github.com>
2024-03-19 11:21:10 +0100

    Creata un interfaccia iniziale e una per client e magazineiere

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,3 @@
-
-import classes.Cart;
-import classes.Product;
-import classes.Warehouse;
-import products.KindOfProduct;
-
 public class Main {
 
     public static void main(String[] args) {
--- src/classes/Console.java
+++ src/classes/Console.java
@@ -1,19 +1,56 @@
 package classes;
 
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-import static classes.Warehouse.*;
+import static classes.Warehouse.fillUpWarehouse;
 
 public class Console {
     public static void main(String[] args) {
         var warehouse = new Warehouse();
+        ArrayList<Cart> carts = new ArrayList<>();
         fillUpWarehouse(warehouse);
-        scnWarehouseManagerHome(warehouse);
+        Scanner scanner = new Scanner(System.in);
+        scnWelcome(scanner, warehouse);
+    }
+
+    public static void scnWelcome(Scanner scanner, Warehouse warehouse) {
+        String input;
+        int intChoice;
+        do {
+            System.out.println("Che interfaccia vuoi utilizzare?\n" +
+                    "0. Cliente 1. Magaziniere Q. Chiudi il programma");
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                return;
+            }
+            intChoice = Integer.parseInt(input);
+        } while ((intChoice < 0) || (intChoice > 1));
+        switch (intChoice) {
+            case 0 -> scnClient(scanner, warehouse);
+            case 1 -> scnWarehouseManagerHome(scanner, warehouse);
+        }
+    }
+
+    public static void scnClient(Scanner scanner, Warehouse warehouse) {
+        String input;
+        int intChoice;
+        do {
+            System.out.println("Che operazione vuoi effettuare?\n" +
+                    "0. Visualizzare il catalogo 1. Aggiungere un prodotto al carrello Q. Chiudi il programma");
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                return;
+            }
+            intChoice = Integer.parseInt(input);
+        } while ((intChoice < 0) || (intChoice > 1));
+        switch (intChoice) {
+            case 0 -> warehouse.printStock();
+            case 1 -> scnWarehouseManagerHome(scanner, warehouse);
+        }
     }
 
     public static products.KindOfProduct scnProductKindChoice(Scanner scanner) {
-
         int input;
         do {
             System.out.println("Che tipo di prodotto (usa i numeri)?\n" +
@@ -32,8 +69,7 @@ public class Console {
     }
 
     //È un metodo che resta in ascolto e ripropone la possibilità di scelte fino a che non è inserita Q
-    public static void scnWarehouseManagerHome(Warehouse warehouse) {
-        Scanner scanner = new Scanner(System.in);
+    public static void scnWarehouseManagerHome(Scanner scanner, Warehouse warehouse) {
         String input;
         do {
             int intChoice;
@@ -67,150 +103,6 @@ public class Console {
         } while (!input.equalsIgnoreCase("q"));
     }
 
-    public static void scnResearchSeller(Map<Product, Integer> warehouse) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        int intChoice;
-        do {
-            System.out.println("""
-                    1.Tipo di Prodotto   \s
-                    2.Produttore  \s
-                    3.Modello   \s
-                    4.ID   \s
-                    5.Prezzo d'acquisto  \s
-                    6.Prezzo di vendita  \s
-                    7.Range di prezzo di acquisto \s
-                    8.Range di prezzo di vendita  \s
-                    e. Torna alla schermata precedente""");
-            input = scanner.nextLine();
-            if (input.equalsIgnoreCase("e")) {
-                return;
-            }
-            while (!input.matches("[1-8]")) {
-                System.out.println("Usa i numeri per effettuare la scelta o 'e' per tornare alla schermata precedente\n");
-                input = scanner.nextLine();
-            }
-            intChoice = Integer.parseInt(input);
-        } while (intChoice < 1 || intChoice > 8);
-        switch (intChoice) {
-            case 1:
-                System.out.println("Seleziona il tipo di articolo che desideri cercare:");
-                checkIfEmptyMap(researchKindOfProduct(warehouse, scanner.nextLine()));
-                askConfirmationEndTaskSeller(warehouse);
-                break;
-
-            case 2:
-                System.out.println("Seleziona il produttore che desideri cercare:");
-                checkIfEmptyMap(researchProducer(warehouse, scanner.nextLine()));
-                askConfirmationEndTaskSeller(warehouse);
-                break;
-            case 3:
-                System.out.println("Seleziona il modello che desideri cercare:");
-                checkIfEmptyMap(researchModel(warehouse, scanner.nextLine()));
-                askConfirmationEndTaskSeller(warehouse);
-                break;
-            case 4:
-                System.out.println("Seleziona l'ID del prodotto che desideri cercare:");
-                checkIfEmptyMap(researchID(warehouse, scanner.nextInt()));
-                askConfirmationEndTaskSeller(warehouse);
-                break;
-
-            case 5:
-                System.out.println("Seleziona il prezzo d'acquisto del prodotto che desideri cercare:");
-                checkIfEmptyMap(researchPurchasePrice(warehouse, scanner.nextDouble()));
-                askConfirmationEndTaskSeller(warehouse);
-                break;
-
-            case 6:
-                System.out.println("Seleziona il prezzo di vendita del prodotto che desideri cercare:");
-                checkIfEmptyMap(researchSellPrice(warehouse, scanner.nextDouble()));
-                askConfirmationEndTaskSeller(warehouse);
-                break;
-
-            case 7:
-                System.out.println("Seleziona il range di prezzo di acquisto dei prodotti che desideri cercare: \nDigita il prezzo minimo che desideri cercare: ");
-                double minPurchasePrice = scanner.nextDouble();
-                System.out.println("Digita il prezzo massimo che desideri cercare: ");
-                double maxPurchasePrice = scanner.nextDouble();
-                checkIfEmptyMap(researchPurchasePriceInRange(warehouse, minPurchasePrice, maxPurchasePrice));
-                askConfirmationEndTaskSeller(warehouse);
-                break;
-
-            case 8:
-                System.out.println("Seleziona il range di prezzo di vendita dei prodotti che desideri cercare: \nDigita il prezzo minimo che desideri cercare: ");
-                double minSellPrice = scanner.nextDouble();
-                System.out.println("Digita il prezzo massimo che desideri cercare: ");
-                double maxSellPrice = scanner.nextDouble();
-                checkIfEmptyMap(researchSellPriceInRange(warehouse, minSellPrice, maxSellPrice));
-                askConfirmationEndTaskSeller(warehouse);
-                break;
-        }
-    }
-
-    public static void scnResearchClient(Map<Product, Integer> warehouse) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        int intChoice;
-        do {
-            System.out.println("""
-                    1.Tipo di Prodotto   \s
-                    2.Produttore  \s
-                    3.Modello   \s
-                    4.ID   \s
-                    5.Prezzo di vendita  \s
-                    6.Range di prezzo di vendita  \s
-                    e. Torna alla schermata precedente""");
-            input = scanner.nextLine();
-            if (input.equalsIgnoreCase("e")) {
-                return;
-            }
-            while (!input.matches("[1-6]")) {
-                System.out.println("Usa i numeri per effettuare la scelta o 'e' per tornare alla schermata precedente\n");
-                input = scanner.nextLine();
-            }
-            intChoice = Integer.parseInt(input);
-        } while (intChoice < 1 || intChoice > 6);
-        switch (intChoice) {
-            case 1:
-                System.out.println("Seleziona il tipo di articolo che desideri cercare:");
-                checkIfEmptyMap(researchKindOfProduct(warehouse, scanner.nextLine()));
-                askConfirmationEndTaskClient(warehouse);
-                break;
-
-            case 2:
-                System.out.println("Seleziona il produttore che desideri cercare:");
-                checkIfEmptyMap(researchProducer(warehouse, scanner.nextLine()));
-                askConfirmationEndTaskClient(warehouse);
-                break;
-            case 3:
-                System.out.println("Seleziona il modello che desideri cercare:");
-                checkIfEmptyMap(researchModel(warehouse, scanner.nextLine()));
-                askConfirmationEndTaskClient(warehouse);
-                break;
-            case 4:
-                System.out.println("Seleziona l'ID del prodotto che desideri cercare:");
-                checkIfEmptyMap(researchID(warehouse, scanner.nextInt()));
-                askConfirmationEndTaskClient(warehouse);
-                break;
-
-            case 5:
-                System.out.println("Seleziona il prezzo del prodotto che desideri cercare:");
-                checkIfEmptyMap(researchSellPrice(warehouse, scanner.nextDouble()));
-                askConfirmationEndTaskClient(warehouse);
-                break;
-
-            case 6:
-                System.out.println("Seleziona il range di prezzo dei prodotti che desideri cercare: \nDigita il prezzo minimo che desideri cercare: ");
-                double minSellPrice = scanner.nextDouble();
-                System.out.println("Digita il prezzo massimo che desideri cercare: ");
-                double maxSellPrice = scanner.nextDouble();
-                checkIfEmptyMap(researchSellPriceInRange(warehouse, minSellPrice, maxSellPrice));
-                askConfirmationEndTaskClient(warehouse);
-                break;
-
-        }
-    }
-
     // Permette all'utente di inserire un prodotto a magazzino tramite un interfaccia da Terminale.
     public static void scnAddProdToWarehouse(Warehouse warehouse) {
         Scanner scanner = new Scanner(System.in);
@@ -234,61 +126,5 @@ public class Console {
         warehouse.addProduct(new Product(article, producer, model, description, display, storage, purchasePrice, sellPrice), quantity);
     }
 
-    public static void fillUpWarehouse(Warehouse warehouse) {
-        warehouse.addProduct(new Product(products.KindOfProduct.NOTEBOOK, "Apple", "M1", "tr", 2.3, 2, 1000, 1250), 10);
-        warehouse.addProduct(new Product(products.KindOfProduct.NOTEBOOK, "Apple", "M2", "tr", 2.3, 2, 1300, 1850), 6);
-        warehouse.addProduct(new Product(products.KindOfProduct.NOTEBOOK, "Asus", "M234", "tr", 2.3, 2, 900, 1450), 8);
-        warehouse.addProduct(new Product(products.KindOfProduct.SMARTPHONE, "Apple", "XR", "tr", 2.3, 2, 250, 400), 10);
-        warehouse.addProduct(new Product(products.KindOfProduct.SMARTPHONE, "Huawei", "Pro2", "tr", 2.3, 2, 100, 200), 6);
-        warehouse.addProduct(new Product(products.KindOfProduct.SMARTPHONE, "Asus", "Pen", "tr", 2.3, 2, 150, 300), 8);
-        warehouse.addProduct(new Product(products.KindOfProduct.TABLET, "Apple", "Ipad", "tr", 2.3, 2, 1000, 1250), 10);
-        warehouse.addProduct(new Product(products.KindOfProduct.TABLET, "Huawei", "Fire", "tr", 2.3, 2, 300, 400), 6);
-        warehouse.addProduct(new Product(products.KindOfProduct.TABLET, "Asus", "Pen", "tr", 2.3, 2, 400, 700), 8);
-    }
 
-    public static void askConfirmationEndTaskSeller(Map<Product, Integer> warehouse) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        int choice;
-        do {
-            System.out.println("Vuoi continuare a cercare? \n0. Sì   1. No");
-            input = scanner.nextLine();
-            while (!input.matches("[0-1]")) {
-                System.out.println("L'input inserito non corrisponde a una delle scelte offerte.");
-                System.out.println("Vuoi continuare a cercare? \n0. Sì   1. No");
-                input = scanner.nextLine();
-            }
-            choice = Integer.parseInt(input);
-        } while (choice < 0 || choice > 1);
-        switch (choice) {
-            case 0:
-                scnResearchSeller(warehouse);
-                break;
-            case 1:
-                break;
-        }
-    }
-
-    public static void askConfirmationEndTaskClient(Map<Product, Integer> warehouse) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        int choice;
-        do {
-            System.out.println("Vuoi continuare a cercare? \n0. Sì   1. No");
-            input = scanner.nextLine();
-            while (!input.matches("[0-1]")) {
-                System.out.println("L'input inserito non corrisponde a una delle scelte offerte.");
-                System.out.println("Vuoi continuare a cercare? \n0. Sì   1. No");
-                input = scanner.nextLine();
-            }
-            choice = Integer.parseInt(input);
-        } while (choice < 0 || choice > 1);
-        switch (choice) {
-            case 0:
-                scnResearchClient(warehouse);
-                break;
-            case 1:
-                break;
-        }
-    }
 }
